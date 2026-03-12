ab544b2560296ce0aaeeeb21b49e87b2651daafc
Mk-mstar <mkperez.contato@gmail.com>
2026-03-12 17:22:09 -0300

    PyInvest (03)
    Terminei hoje, adicionei os blocos pedidos e organizei o código

--- PyInvest.py
+++ PyInvest.py
@@ -17,14 +17,14 @@ taxa_fii = float(input('Rentabilidade mensal FII (%): '))/100
 Meta = float(input('Projeção financeira (R$): '))
 
 #Processamento
-cdi_mensal =  math.pow((1+cdi_anual, 1/12) ) -1 
+cdi_mensal = math.pow(1 + cdi_anual, 1/12) - 1
 
 #Total investido 
 total_inv = capital + (aporte * meses)
 
 #CDB
 taxa_cdb = cdi_mensal * perc_cdb
-montante_cdb =( (capital * math.pow(1 + taxa_cdb), meses ) + (aporte * meses))
+montante_cdb = (capital * math.pow(1 + taxa_cdb, meses ) + (aporte * meses))
 lucro_cdb = montante_cdb - total_inv
 montante_cdb_liquido = total_inv + (lucro_cdb * 0.85)
 
@@ -34,7 +34,50 @@ montante_lci = (capital * math.pow((1 + taxa_lci), meses) + (aporte * meses))
 
 #Poupança
 taxa_poup = 0.005 
-montante_poup = ( capital * math.pow((1 + taxa_poup), meses + (aporte * meses)) )
+montante_poup = capital * (1 + taxa_poup) ** meses
 
 #FII - Simulações
+variacao =  random.uniform(-3, 3) 
+variacao2 =  random.uniform(-3, 3) 
+variacao3 =  random.uniform(-3, 3) 
+variacao4 =  random.uniform(-3, 3) 
+variacao5 =  random.uniform(-3, 3) 
+fii1 = capital * (1 + variacao)
+fii2 = capital * (1 + variacao2)
+fii3 = capital * (1 + variacao3)
+fii4 = capital * (1 + variacao4)
+fii5 = capital * (1 + variacao5)
 
+#Formação Mónetaria
+media = statistics.mean([fii1, fii2, fii3, fii4, fii5])
+desvio_padrao = statistics.stdev([fii1, fii2, fii3, fii4, fii5])
+valores = [fii1, fii2, fii3, fii4, fii5]
+mediana = statistics.median(valores)
+
+#Data Resgate
+data_compra = datetime.datetime.strptime(input('Data de compra (dd/mm/yyyy): '), '%d/%m/%Y')
+resgate = data_compra + datetime.timedelta(days = meses)
+
+#Saídas
+
+print (f'Data da simulação: {datetime.datetime.now().strftime("%d/%m/%Y")}')
+print (f'Resgate previsto para: {resgate.strftime("%d/%m/%Y")}')
+print(f'Total investido: { total_inv:.0f}') 
+
+print(f'CDB: R$ {montante_cdb:.2f}') 
+print("CDB:", "█" * int(montante_cdb / 1000))
+
+print(f'LCI: R$ {montante_lci:.2f}') 
+print("LCI/LCA:", "█" * int(montante_lci / 1000))
+
+print(f'Poupança: R$ {montante_poup:.2f}') 
+print("Poupança:", "█" * int(montante_poup / 1000))
+
+print(f'FII: R$ {media:.2f}') 
+print("FII:", "█" * int(media / 1000))
+
+print(f'Mediana FII R$ {mediana:.2f}')
+print(f'Desvio Padrão FII: {desvio_padrao:.2f}')
+
+meta_batida = mediana >= Meta
+print(f"Meta batida: {meta_batida}")
